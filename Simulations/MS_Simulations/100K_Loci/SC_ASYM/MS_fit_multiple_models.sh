a5c414c2295ae31f574e3f8f53cf90bb0edbbb66
Nopaoli <Nopaoli@users.noreply.github.com>
2020-05-26 12:14:57 +0300

    Add files via upload

---
+++ Simulations/MS_Simulations/100K_Loci/SC_ASYM/MS_fit_multiple_models.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -l
+# author: momiglia
+#SBATCH --constraint="snb|hsw"
+#SBATCH -o SC_fit.ou
+#SBATCH -e SC_fit.err
+#SBATCH -p serial
+#SBATCH -n 1
+#SBATCH -t 72:00:00
+#SBATCH --mem-per-cpu=9000
+
+
+
+module load bioconda
+source activate moments
+export PYTHONPATH=$PYTHONPATH:$HOME/appl_taito/moments
+
+cd /wrk/momiglia/DONOTREMOVE/MS_fit/2b_SIM/SC/64_ASYM/
+while read a b c d e f g h k l n; do $b $c $d $e $f $g $h $k $l $n > "$a".sim; done < SC_MS_TABLE.txt
---
+++ Simulations/MS_Simulations/1_Million_Loci/IM_ASYM/MS_fit_multiple_models.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -l
+# author: momiglia
+#SBATCH --constraint="snb|hsw"
+#SBATCH -o MS_fit.ou
+#SBATCH -e MS_fit.err
+#SBATCH -p serial
+#SBATCH -n 1
+#SBATCH -t 72:00:00
+#SBATCH --mem-per-cpu=9000
+
+
+
+module load bioconda
+source activate moments
+export PYTHONPATH=$PYTHONPATH:$HOME/appl_taito/moments
+
+cd /wrk/momiglia/DONOTREMOVE/MS_fit/2b_SIM/1_Mill/64_ASYM
+while read a b c d e f g h k l; do $b $c $d $e $f $g $h $k $l > "$a".sim; done < IM_MS_TABLE.txt
